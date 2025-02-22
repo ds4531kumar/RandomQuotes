3366eda987e51c65b2c62117523bae6fdb754122
ds4531kumar <ds4531>
2025-02-22 14:45:47 +0530

    Add Project

---
+++ RandomQuotes/RandomQuotes.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace RandomQuotes.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int add = 6 + 8;
+            Assert.AreEqual(14, add);
+        }
+    }
+}
